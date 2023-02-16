9d5dd5f31c030272482a606131846d54f3502645
Sean Burkard <skburkard@crimson.ua.edu>
2023-02-16 14:56:48 -0600

    Changes

---
+++ mis-42-assignment-2-jquery-and-ajax-SeanBurkard/wwwroot/script.js
@@ -0,0 +1,46 @@
+var len;
+var results = '';
+
+function apiSearch() {
+  var params = {
+    "q": $("#query").val(),
+    "count": "50",
+    "offset": "0",
+    "mkt": "en-us"
+  };
+
+  $.ajax({
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+      beforeSend: function (xhrObj) {
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "f9b24b05b0a248edbc80460bf98b3cd2");
+      },
+      type: "GET",
+    })
+    .done(function (data) {
+      len = data.webPages.value.length;
+      for (i = 0; i < len; i++) {
+        results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+      }
+
+      $('#searchResults').html(results);
+      $('#searchResults').dialog();
+    })
+    .fail(function () {
+      alert("error");
+    });
+}
+
+function formatTime(date) {
+    var hour = date.getHours();
+    var min = date.getMinutes();
+
+    if (hour.toString().length == 1) {
+        hour = '0' + hour;
+    }
+
+    if (min.toString().length == 1) {
+        min = '0' + min;
+    }
+
+    return hour + ':' + min;
+}
